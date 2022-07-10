b3eb678f49e016fb2085c80cbc5e2f3ecf22a235
AbhirathSP <abhirathsp2@gmail.com>
2022-07-10 17:11:42 +0530

    Initial Commit

---
+++ Bucks/BucksApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucksApp.swift
+//  Bucks
+//
+//  Created by Abhirath Sujith on 10/07/22.
+//
+
+import SwiftUI
+
+@main
+struct BucksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bucks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bucks
+//
+//  Created by Abhirath Sujith on 10/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
